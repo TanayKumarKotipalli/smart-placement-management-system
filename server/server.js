e23834befc893f25baea962badb5fd8d8ebd4cba
K.Tanay Kumar <tanaymother6655@gmail.com>
2026-03-15 18:55:20 +0530

    Fix render port issue

--- server/server.js
+++ server/server.js
@@ -20,12 +20,12 @@ app.use("/api/admin", require("./routes/admin"));
 app.get("/", (req, res) => {
   res.send("Smart Placement API Running");
 });
-
+const PORT = process.env.PORT || 5000;
 mongoose.connect(process.env.MONGO_URI)
   .then(() => {
     console.log("MongoDB Connected");
-    app.listen(5000, () => {
-      console.log("Server running on port 5000");
+    app.listen(PORT, () => {
+      console.log('Server running on port ${PORT}');
     });
   })
   .catch((err) => console.log(err));
